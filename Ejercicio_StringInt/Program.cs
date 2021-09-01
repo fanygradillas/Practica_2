48b8eada06658eb6195b39df52d6dae7066ad6fc
Estefania Lopez Gradillas <fanygradillas@hotmail.com>
2021-08-31 20:20:40 -0700

    Ejercicio_StringInt

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            int edad;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Te llamas " + nombre + " y tienes " + edad + " años");
+            Console.ReadKey();
+        }
+    }
+}
